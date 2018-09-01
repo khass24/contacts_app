c0e668430ca583c12d76d096839d67d607a1a525
Kyle Hassett <kylehassett72@gmail.com>
2018-09-01 12:28:04 -0500

    RESTful routes controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -22,8 +22,20 @@ class Api::ContactsController < ApplicationController
   end
 
   def update
+    @contact = Contact.find(params[:id])
+
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+
+    @contact.save
+    render "show.json.jbuilder"
   end
 
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render json: {message: "Contact successfully destroyed"}
   end
 end
